8ffaf11c8c57218508731fbc6368a30699aab472
leesj0188 <lsj01881@gmail.com>
2024-11-12 18:38:59 +0900

    1차 수정

--- week06/이성진/todo/src/App.jsx
+++ week06/이성진/todo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+/*import { useState, useEffect } from 'react';
 import TodoBoard from './component/TodoBoard.jsx';
 import './App.css';
 
@@ -53,4 +53,67 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+import { useState, useEffect } from 'react';
+import TodoBoard from './component/TodoBoard.jsx';
+import './App.css';
+
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todolist, setTodolist] = useState(() => {
+    const savedTodos = localStorage.getItem('todolist');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('todolist', JSON.stringify(todolist));
+    console.log('Todo 리스트가 업데이트되었습니다:', [todolist]);
+  }, [todolist]);
+
+  const addItem = () => {
+    if (inputValue.trim()) {
+      const newTodo = {
+        id: Date.now(),
+        text: inputValue,
+        isCompleted: false
+      };
+      setTodolist((prevTodolist) => [...prevTodolist, newTodo]);
+      setInputValue('');
+    }
+  };
+
+  const deleteItem = (idToDelete) => {
+    setTodolist(todolist.filter((item) => item.id !== idToDelete));
+  };
+
+  const toggleComplete = (idToToggle) => {
+    setTodolist(todolist.map((item) => 
+      item.id === idToToggle ? { ...item, isCompleted: !item.isCompleted } : item
+    ));
+  };
+
+  return (
+    <div className="bg-neutral-50 rounded-lg p-8 shadow-md ">
+      <h1 className="font-mono text-center text-3xl mb-6 text-gray-700 shadow-md rounded-full">TODO LIST</h1>
+      <div className="flex mb-6">
+        <input
+          className="text-gray-800 placeholder-gray-500 text-center rounded-lg px-4 py-2 mr-2 shadow-sm focus:outline-none"
+          value={inputValue}
+          type="text"
+          placeholder="해야할 일을 입력하세요"
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button
+          className="bg-slate-50 text-black rounded-lg px-4 py-2 hover:bg-slate-200 shadow-md hover:shadow-sm"
+          onClick={addItem}
+        >
+          추가
+        </button>
+      </div>
+      <TodoBoard todolist={todolist} deleteItem={deleteItem} toggleComplete={toggleComplete} />
+    </div>
+  );
+}
+
+export default App
--- week07/이성진/todo/src/component/TodoBoard.jsx
+++ week07/이성진/todo/src/component/TodoBoard.jsx
@@ -1,17 +1,3 @@
-/*import React from "react";
-import TodoItem from "./TodoItem";
-
-function TodoBoard({ todolist, deleteItem }) {
-  return (
-    <div className="space-y-3.5">
-      {todolist.map((item) => (
-        <TodoItem key={item.id} item={item} deleteItem={deleteItem} />
-      ))}
-    </div>
-  );
-}
-
-export default TodoBoard;*/
 import React from "react";
 import TodoItem from "./TodoItem";
 
