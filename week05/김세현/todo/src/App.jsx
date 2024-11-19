92ef4d67cdc5d753592c1ecdff48cf5de9e5a12f
sehyun0329 <tpgus4857@naver.com>
2024-11-19 16:56:35 +0900

    김세현 5,6,7 주차 파일들 다시 커밋 (11.19)

---
+++ week05/김세현/todo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './TodoList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
