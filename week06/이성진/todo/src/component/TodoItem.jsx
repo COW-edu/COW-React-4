4c7114ab090974f028e09f24140b735303e214b6
leesj0188 <lsj01881@gmail.com>
2024-11-20 13:59:00 +0900

    za

--- week06/이성진/todo/src/component/TodoItem.jsx
+++ week06/이성진/todo/src/component/TodoItem.jsx
@@ -1,26 +1,8 @@
-/*import React from "react";
-
-function TodoItem({ item, deleteItem }) {
-  return (
-    <div className="flex justify-between items-center bg-pastel-blue rounded-md px-3 py-2 shadow-sm hover:shadow-md text-sm">
-      <span className="text-gray-800 truncate">{item.text}</span>
-      <button
-        className="text-black bg-transparent hover:bg-slate-200 rounded-full p-1 transition-colors text-xs"
-        onClick={() => deleteItem(item.id)}
-      >
-        X
-      </button>
-    </div>
-  );
-}
-
-export default TodoItem;*/
-
 import React from "react";
 
 function TodoItem({ item, deleteItem, toggleComplete }) {
   return (
-    <div className={`flex justify-between items-center bg-pastel-blue rounded-md px-3 py-2 shadow-sm hover:shadow-md text-sm ${item.isCompleted ? 'bg-green-100' : ''}`}>
+    <div className={"flex justify-between items-center bg-pastel-blue rounded-md px-3 py-2 shadow-sm hover:shadow-md text-sm ${item.isCompleted ? 'bg-green-100' : ''}"}>
       <span className={`text-gray-800 truncate ${item.isCompleted ? 'line-through' : ''}`}>{item.text}</span>
       <div>
         <button
