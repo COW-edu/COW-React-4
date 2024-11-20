95fa5c25e30147a6821bee17e171f214f2ef9dee
최원빈 <choewonbin@Binz-MacBook-Air-M2-15.local>
2024-11-20 21:32:25 +0900

    push at 21:32pm

--- week08/최원빈/todoList/src/Auth/LoginForm.jsx
+++ week08/최원빈/todoList/src/Auth/LoginForm.jsx
@@ -27,7 +27,7 @@ function LoginForm() {
         localStorage.setItem('token', response.token); // 로그인 성공 시 토큰 저장
         navigate('/todos'); // Todos 페이지로 이동
       } catch (error) {
-        console.error('로그인 실패:', error);
+        console.error('로그인 실패 시발:', error);
         alert('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
       }
     }
